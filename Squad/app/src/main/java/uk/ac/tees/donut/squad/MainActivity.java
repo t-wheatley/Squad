44a81b01d9a64d89b7fc6b7b5497ca5ef7fff519
t-wheatley <wheatley.t07@gmail.com>
2017-03-03 18:52:22 +0000

    Initial Commit
    Empty Android project.

---
+++ Squad/app/src/main/java/uk/ac/tees/donut/squad/MainActivity.java
@@ -0,0 +1,15 @@
+package uk.ac.tees.donut.squad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
