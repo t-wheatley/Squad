9c5adf9fb1303cdff934bd816cb8d535413c1814
08collingsj <jlc-1996@live.co.uk>
2017-03-21 14:43:00 +0000

    Some visual changes
    Splash screen added (Logo src needs adding)

--- app/src/main/java/uk/ac/tees/donut/squad/MainActivity.java
+++ app/src/main/java/uk/ac/tees/donut/squad/MainActivity.java
@@ -3,6 +3,7 @@ package uk.ac.tees.donut.squad;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 
