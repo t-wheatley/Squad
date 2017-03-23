4e6a3d6dfab20572e01c90e1e632675ad1f6c246
t-wheatley <wheatley.t07@gmail.com>
2017-03-23 19:49:41 +0000

    Changed the OAuth 2.0 client ID
    Changed the default

--- app/src/main/java/uk/ac/tees/donut/squad/SignInActivity.java
+++ app/src/main/java/uk/ac/tees/donut/squad/SignInActivity.java
@@ -51,7 +51,7 @@ public class SignInActivity extends AppCompatActivity implements
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.web_client_id))
                 .requestEmail()
                 .build();
 
@@ -127,7 +127,7 @@ public class SignInActivity extends AppCompatActivity implements
                 handleSignInResult(result);
             } else {
                 // Google Sign In failed, update UI appropriately
-                // ...
+                statusTextView.setText("Google Sign-In failed.");
             }
         }
     }
