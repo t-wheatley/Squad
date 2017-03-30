2d7458b01284523c1386c436b291cd5ad6d14904
Turnipmuncher <q5233000@live.tees.ac.uk>
2017-03-30 22:41:17 +0100

    minor changes

--- app/src/main/java/uk/ac/tees/donut/squad/location/MapActivity.java
+++ app/src/main/java/uk/ac/tees/donut/squad/location/MapActivity.java
@@ -1,8 +1,12 @@
 package uk.ac.tees.donut.squad.location;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,7 +16,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import uk.ac.tees.donut.squad.R;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback,  GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
 
@@ -34,6 +39,22 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         LatLng Teesside = new LatLng(54.572872, -1.234802);
         mMap.addMarker(new MarkerOptions().position(Teesside).title("Marker in Teesside"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Teesside));
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 }
