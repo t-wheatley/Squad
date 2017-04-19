127e68666ab21fea02df72554ebab0f0b725eb52
luongalex <alexluong@hotmail.co.uk>
2017-04-20 00:44:02 +0100

    Squad UI

--- app/src/main/java/uk/ac/tees/donut/squad/activities/SquadsActivity2.java
+++ app/src/main/java/uk/ac/tees/donut/squad/activities/SquadsActivity2.java
@@ -2,10 +2,6 @@ package uk.ac.tees.donut.squad.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import uk.ac.tees.donut.squad.R;
 
@@ -15,11 +11,5 @@ public class SquadsActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_squads2);
-    }
-
-    public void buttonOnClick(View v){
-        ImageButton test = (ImageButton)findViewById(R.id.videoGamesBtn);
-        TextView test2 =(TextView)findViewById(R.id.textviewAS);
-        test2.setText(String.valueOf(test.getMeasuredWidth()));
     }
 }
