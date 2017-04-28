12c4f3e3a906806e89aedbc8cdd0f20d06557982
luongalex <alexluong@hotmail.co.uk>
2017-04-28 16:41:58 +0100

    ?

--- app/src/main/java/uk/ac/tees/donut/squad/activities/SquadPostActivity.java
+++ app/src/main/java/uk/ac/tees/donut/squad/activities/SquadPostActivity.java
@@ -2,26 +2,23 @@ package uk.ac.tees.donut.squad.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
+import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
 import uk.ac.tees.donut.squad.R;
 
 public class SquadPostActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Button btnPost;
+    private MultiAutoCompleteTextView txtbox;
 
-    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_squad_post);
 
-        // Getting the reference for the Firebase Realtime Database
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        txtbox = (MultiAutoCompleteTextView) findViewById(R.id.txtboxPost);
+
     }
 }
