fcf6f56bbbf123249497c97318ab170416f90b47
08collingsj <jlc-1996@live.co.uk>
2017-05-02 15:00:57 +0100

    Jacks new toys

---
+++ app/src/main/java/uk/ac/tees/donut/squad/activities/fragments/ListFragment.java
@@ -0,0 +1,38 @@
+package uk.ac.tees.donut.squad.activities.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+/**
+ * Created by jlc-1 on 02/05/2017.
+ */
+
+public class ListFragment extends Fragment implements AdapterView.OnItemClickListener
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.list_fragment, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.Planets, android.R.layout.simple_list_item_1);
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+    }
+}
