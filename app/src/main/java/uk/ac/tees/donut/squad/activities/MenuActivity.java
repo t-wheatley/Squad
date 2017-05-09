060c8f1567e58521ffa76a267bfdb8958d0225d4
08collingsj <jlc-1996@live.co.uk>
2017-05-09 14:46:54 +0100

    stuff

--- app/src/main/java/uk/ac/tees/donut/squad/activities/MenuActivity.java
+++ app/src/main/java/uk/ac/tees/donut/squad/activities/MenuActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -24,21 +25,26 @@ import uk.ac.tees.donut.squad.fragments.SquadFragment;
 import uk.ac.tees.donut.squad.location.GeocoderActivity;
 import uk.ac.tees.donut.squad.location.MapActivity;
 
-public class MenuActivity extends AppCompatActivity
+public class MenuActivity extends BaseActivity
 {
     FirebaseUser firebaseUser;
-    private static final String SELECTED_ITEM = "arg_selected_item";
-
-    private BottomNavigationView mBottomNav;
-    private int mSelectedItem;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
     }
+
+    @Override
+    int getContentViewId() {
+        return R.layout.activity_menu;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.menu_home;
+    }
+
 
     //Click functionality
     public void openProfile(View view)
@@ -55,18 +61,21 @@ public class MenuActivity extends AppCompatActivity
     public void openSquads(View view)
     {
         Intent intent = new Intent(this, SquadListActivity.class);
+        System.out.println("------ Opened Squads ------");
         startActivity(intent);
     }
 
     public void openEvents(View view)
     {
         Intent intent = new Intent(this, MeetupsListActivity.class);
+        System.out.println("------ Opened Meetups ------");
         startActivity(intent);
     }
 
     public void openHost(View view)
     {
         Intent intent = new Intent(this, NewMeetupActivity.class);
+        System.out.println("------ Opened New Meetup ------");
         startActivity(intent);
     }
 
@@ -97,102 +106,7 @@ public class MenuActivity extends AppCompatActivity
     // USE THIS WHEN YOU NEED TO DEBUG OR TEST CERTAIN THINGS WITHOUT DESTROYING THE REST OF THE APP
     public void openMystery(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mBottomNav = (BottomNavigationView) findViewById(R.id.nav_bar);
-        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                selectFragment(item);
-                return true;
-            }
-        });
-
-        MenuItem selectedItem;
-        if (savedInstanceState != null) {
-            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
-            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
-        } else {
-            selectedItem = mBottomNav.getMenu().getItem(0);
-        }
-        selectFragment(selectedItem);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putInt(SELECTED_ITEM, mSelectedItem);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onBackPressed() {
-        MenuItem homeItem = mBottomNav.getMenu().getItem(0);
-        if (mSelectedItem != homeItem.getItemId()) {
-            // select home item
-            selectFragment(homeItem);
-        } else {
-            super.onBackPressed();
-        }
-    }
-    private void fragmentManager(Fragment fragment) {
-        if (fragment == null)
-            return;
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        if (fragmentManager != null) {
-            FragmentTransaction ft = fragmentManager.beginTransaction();
-            if (ft != null) {
-                ft.replace(R.id.container, fragment);
-                ft.commit();
-            }
-        }
-    }
-
-    private void selectFragment(MenuItem item) {
-        Fragment frag = null;
-        // init corresponding fragment
-        switch (item.getItemId()) {
-            case R.id.menu_home:
-                System.out.println("-------Selected: Home Fragment-------");
-                fragmentManager(new HomeFragment());
-                break;
-            case R.id.menu_squads:
-                System.out.println("-------Selected: Squads Fragment-------");
-                fragmentManager(new SquadFragment());
-                break;
-            case R.id.menu_meetups:
-                System.out.println("-------Selected: Meetups Fragment-------");
-                fragmentManager(new MeetupFragment());
-                break;
-            case R.id.menu_profile:
-                System.out.println("-------Selected: Profile Fragment-------");
-                fragmentManager(new ProfileFragment());
-                break;
-        }
-
-        // update selected item
-        mSelectedItem = item.getItemId();
-
-        // uncheck the other items.
-        for (int i = 0; i< mBottomNav.getMenu().size(); i++) {
-            MenuItem menuItem = mBottomNav.getMenu().getItem(i);
-            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
-        }
-
-        updateToolbarText(item.getTitle());
-
-        if (frag != null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.add(R.id.container, frag, frag.getTag());
-            ft.commit();
-        }
-    }
-
-    private void updateToolbarText(CharSequence text) {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle(text);
-        }
+        //Intent intent = new Intent(this, BaseActivity.class);
+        //startActivity(intent);
     }
 }
