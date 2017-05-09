413ed84dd2c2b706b1f8243abc5a6258760140a1
t-wheatley <q5071488@apple39.scm.tees.ac.uk>
2017-05-09 13:48:41 +0100

    Splash Screen
    Got rid of the ugly white cold start

---
+++ app/src/main/java/uk/ac/tees/donut/squad/activities/SplashScreen.java
@@ -0,0 +1,17 @@
+package uk.ac.tees.donut.squad.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
