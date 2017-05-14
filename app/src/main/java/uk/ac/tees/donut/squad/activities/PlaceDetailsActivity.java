de4abd4beb4d069ca95fd319f482a2f31d5308f8
MrScottyTay <mrscottytaylor@gmail.com>
2017-05-14 11:40:35 +0100

    place Details finish (i think)

--- app/src/main/java/uk/ac/tees/donut/squad/activities/PlaceDetailsActivity.java
+++ app/src/main/java/uk/ac/tees/donut/squad/activities/PlaceDetailsActivity.java
@@ -3,12 +3,14 @@ package uk.ac.tees.donut.squad.activities;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -51,7 +53,11 @@ public class PlaceDetailsActivity extends BaseActivity
     double longitude;
 
     ImageSwitcher gallery;
-    RelativeLayout galleryLayout;
+
+    boolean burger = false;
+    FloatingActionButton fab;
+    LinearLayout burgerMenu;
+    LinearLayout hostMenu;
 
 
     @Override
@@ -95,6 +101,10 @@ public class PlaceDetailsActivity extends BaseActivity
 
         gallery = (ImageSwitcher) findViewById(R.id.placeDetails_gallery);
 
+        fab = (FloatingActionButton) findViewById(R.id.placeDetails_fab);
+        burgerMenu = (LinearLayout) findViewById(R.id.placeDetails_burgerMenu);
+        hostMenu = (LinearLayout) findViewById(R.id.placeDetails_hostMenu);
+
         //if there are no pictures
         boolean noPics = true; //TEMPORARY TILL WE CAN ATTEMPT AT LOADING PICS
         if (noPics)
@@ -107,9 +117,6 @@ public class PlaceDetailsActivity extends BaseActivity
             noPic.setVisibility(View.GONE);
             gallery.setVisibility(View.VISIBLE);
         }
-
-
-
 
         //gets extras passd from last activity
         Intent detail = getIntent();
@@ -237,4 +244,21 @@ public class PlaceDetailsActivity extends BaseActivity
         intent.putExtra("placeId", placeId);
         startActivity(intent);
     }
+
+    public void fab(View view)
+    {
+        if(burger == false)
+        {
+            burger = true;
+            burgerMenu.setVisibility(View.VISIBLE);
+            fab.setImageResource(R.drawable.ic_cross);
+        }
+        else
+        {
+            burger = false;
+            burgerMenu.setVisibility(View.GONE);
+            fab.setImageResource(R.drawable.ic_burger);
+        }
+    }
+
 }
