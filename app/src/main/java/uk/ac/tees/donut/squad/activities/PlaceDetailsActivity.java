4a4ef2f6baf99c744cd0302a16e18a28b0ca6d80
luongalex <alexluong@hotmail.co.uk>
2017-05-16 15:00:26 +0100

    Merge branch 'master' into Alex4
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/uk/ac/tees/donut/squad/activities/PlaceDetailsActivity.java
+++ app/src/main/java/uk/ac/tees/donut/squad/activities/PlaceDetailsActivity.java
@@ -2,16 +2,31 @@ package uk.ac.tees.donut.squad.activities;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,17 +36,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.mvc.imagepicker.ImagePicker;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import uk.ac.tees.donut.squad.R;
-import uk.ac.tees.donut.squad.location.PlaceMapsActivity;
 import uk.ac.tees.donut.squad.posts.LocPlace;
 
 /**
  * Activity which allows the user to view the details of a Place.
  */
 
-public class PlaceDetailsActivity extends BaseActivity
-{
+public class PlaceDetailsActivity extends BaseActivity {
     // Firebase
     DatabaseReference mDatabase;
     FirebaseUser firebaseUser;
@@ -41,6 +60,7 @@ public class PlaceDetailsActivity extends BaseActivity
     // Loading Overlay
     RelativeLayout loadingOverlay;
     TextView loadingText;
+    ProgressBar imageLoading;
 
     // Activity UI
     TextView placeName;
@@ -62,16 +82,18 @@ public class PlaceDetailsActivity extends BaseActivity
     LocPlace place;
     double latitude;
     double longitude;
+    ArrayList<String> images;
+    int imagePosition;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_details);
 
         // Initialising loading overlay and displaying
         loadingOverlay = (RelativeLayout) this.findViewById(R.id.loading_overlay);
         loadingText = (TextView) this.findViewById(R.id.loading_overlay_text);
+        imageLoading = (ProgressBar) findViewById(R.id.placeDetails_imageProgress);
         loadingText.setText("Loading Place...");
         loadingOverlay.setVisibility(View.VISIBLE);
 
@@ -85,34 +107,34 @@ public class PlaceDetailsActivity extends BaseActivity
         burgerMenu = (LinearLayout) findViewById(R.id.placeDetails_burgerMenu);
         hostMenu = (LinearLayout) findViewById(R.id.placeDetails_hostMenu);
         mapBtn = (Button) findViewById(R.id.mapButton);
-        mapBtn.setOnClickListener(new View.OnClickListener()
-        {
+        mapBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 openMapLocation();
             }
         });
         meetupsBtn = (Button) findViewById(R.id.meetupsButton);
-        meetupsBtn.setOnClickListener(new View.OnClickListener()
-        {
+        meetupsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 viewMeetups();
             }
         });
 
         gallery = (ImageSwitcher) findViewById(R.id.placeDetails_gallery);
+        gallery.setFactory(new ViewSwitcher.ViewFactory() {
+
+            public View makeView() {
+                return new ImageView(PlaceDetailsActivity.this);
+            }
+        });
 
         //if there are no pictures
         boolean noPics = true; //TEMPORARY TILL WE CAN ATTEMPT AT LOADING PICS
-        if (noPics)
-        {
+        if (noPics) {
             //keeps the noPic text, and changes the height of the layout so it's not too big
             gallery.setVisibility(View.GONE);
-        } else
-        {
+        } else {
             //gets rid of the noPic text
             noPic.setVisibility(View.GONE);
             gallery.setVisibility(View.VISIBLE);
@@ -124,20 +146,16 @@ public class PlaceDetailsActivity extends BaseActivity
         // Gets extras passd from last activity
         Intent detail = getIntent();
         Bundle b = detail.getExtras();
-        if (b != null)
-        {
+        if (b != null) {
             placeId = (String) b.get("placeId");
             this.setTitle("Place Details");
-        } else
-        {
+        } else {
             new AlertDialog.Builder(PlaceDetailsActivity.this)
                     .setTitle("Error")
                     .setMessage("The place went missing somewhere, please try again")
-                    .setPositiveButton("Back", new DialogInterface.OnClickListener()
-                    {
+                    .setPositiveButton("Back", new DialogInterface.OnClickListener() {
                         @Override
-                        public void onClick(DialogInterface dialog, int which)
-                        {
+                        public void onClick(DialogInterface dialog, int which) {
                             finish();
                         }
                     })
@@ -151,34 +169,32 @@ public class PlaceDetailsActivity extends BaseActivity
         // Getting the reference for the Firebase Storage
         firebaseStorage = FirebaseStorage.getInstance();
 
+        images = new ArrayList<String>();
+        imagePosition = 0;
+
         // Starts the loading chain
-        // loadMeetup -> loadSquad
+        // loadMeetup -> loadSquad -> loadPictures
         loadPlace();
     }
 
     @Override
-    int getContentViewId()
-    {
+    int getContentViewId() {
         return R.layout.activity_place_details;
     }
 
     @Override
-    int getNavigationMenuItemId()
-    {
+    int getNavigationMenuItemId() {
         return R.id.menu_places;
     }
 
     /**
      * Uses the placeId to create a Place object and display its details.
      */
-    public void loadPlace()
-    {
+    public void loadPlace() {
         // Reads the data from the placeId in Firebase
-        mDatabase.child("places").child(placeId).addListenerForSingleValueEvent(new ValueEventListener()
-        {
+        mDatabase.child("places").child(placeId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot)
-            {
+            public void onDataChange(DataSnapshot dataSnapshot) {
                 // Gets the data from Firebase and stores it in a Place class
                 place = dataSnapshot.getValue(LocPlace.class);
 
@@ -189,13 +205,18 @@ public class PlaceDetailsActivity extends BaseActivity
                 longitude = place.getLocLong();
                 latitude = place.getLocLat();
 
+                // If signed-in user is the Host of the Meetup
+                if (firebaseUser.getUid().equals(place.getHost())) {
+                    // Display editing controls
+                    editMode();
+                }
+
                 // Load the name of the Squad
                 loadSquad();
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError)
-            {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
@@ -204,37 +225,174 @@ public class PlaceDetailsActivity extends BaseActivity
     /**
      * Uses the squadId of the Meetup to load the name of the Squad it belongs to.
      */
-    public void loadSquad()
-    {
+    public void loadSquad() {
         // Setting the loading text
         loadingText.setText("Getting the Place's Squad...");
 
         // Get Squad name from id
-        mDatabase.child("squads").child(place.getSquad()).addListenerForSingleValueEvent(new ValueEventListener()
-        {
+        mDatabase.child("squads").child(place.getSquad()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot)
-            {
+            public void onDataChange(DataSnapshot dataSnapshot) {
                 // Displays the Squad's name
                 squad.setText(dataSnapshot.child("name").getValue(String.class));
 
-                // Hiding loading overlay
-                loadingOverlay.setVisibility(View.GONE);
+                loadPictures();
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError)
-            {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
     }
 
     /**
+     * Method to download and display the Place's pictures.
+     */
+    public void loadPictures() {
+        HashMap<String, String> pictures = place.getPictures();
+
+        if (pictures != null) {
+            noPic.setVisibility(View.GONE);
+            gallery.setVisibility(View.VISIBLE);
+
+            for (final String pictureUrl : pictures.values()) {
+                images.add(pictureUrl);
+
+            }
+
+            displayImage(images.get(0));
+
+
+            // Hiding loading overlay
+            loadingOverlay.setVisibility(View.GONE);
+        } else {
+            // No pictures
+            // Hiding loading overlay
+            loadingOverlay.setVisibility(View.GONE);
+        }
+    }
+
+    public void displayImage(String pictureUrl) {
+        imageLoading.setVisibility(View.VISIBLE);
+
+        // Download and display using Glide
+        Glide.with(PlaceDetailsActivity.this)
+                .load(pictureUrl)
+                .asBitmap()
+                .listener(new RequestListener<String, Bitmap>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<Bitmap> target, boolean isFirstResource) {
+                        Toast.makeText(PlaceDetailsActivity.this, "Something went wrong loading the image", Toast.LENGTH_SHORT);
+                        imageLoading.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, String model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        gallery.setImageDrawable(new BitmapDrawable(getResources(), resource));
+                        imageLoading.setVisibility(View.GONE);
+                        return true;
+                    }
+                })
+                .into((ImageView) gallery.getCurrentView());
+    }
+
+    public void nextImage(View view) {
+        imagePosition++;
+        if (imagePosition == images.size()) {
+            imagePosition = 0;
+        }
+        displayImage(images.get(imagePosition));
+    }
+
+
+    /**
+     * Method called on result of the ImagePicker Intent.
+     *
+     * @param requestCode The request code that was passed to startActivityForResult().
+     * @param resultCode  The result code, RESULT_OK if successful, RESULT_CANCELED if not.
+     * @param data        An Intent that carries the data of the result.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Getting a bitmap from the Intent
+        final Bitmap bitmap = ImagePicker.getImageFromResult(this, requestCode, resultCode, data);
+
+        // The user's id and current time in millis is used to create a unique id
+        final String uniqueId = firebaseUser.getUid() + System.currentTimeMillis();
+
+        // Creating the reference for the meetup's Firebase Storage, used to store pictures,
+        placeStorage = firebaseStorage.getReference().child("places/" + place.getPlaceId() + "/" +
+                uniqueId);
+
+        // If Bitmap exists
+        if (bitmap != null) {
+            loadingText.setText("Uploading photo...");
+            loadingOverlay.setVisibility(View.VISIBLE);
+
+            // Creating a ByteArray from the bitmap
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] bytes = baos.toByteArray();
+
+            // Upload to Firebase Storage
+            UploadTask uploadTask = placeStorage.putBytes(bytes);
+
+            // Upload Listener
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // If upload failed
+                    loadingOverlay.setVisibility(View.GONE);
+                    Toast.makeText(PlaceDetailsActivity.this, "Upload failed, please try again!"
+                            , Toast.LENGTH_SHORT);
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // If upload successful
+                    // Push the download URL to the Place's pictures
+                    final Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    mDatabase.child("places").child(placeId).child("pictures").child(uniqueId).setValue(downloadUrl.toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // Notify user
+                            loadingOverlay.setVisibility(View.GONE);
+                            Toast.makeText(PlaceDetailsActivity.this, "Photo uploaded!", Toast.LENGTH_SHORT);
+
+                            if (images.isEmpty()) {
+                                gallery.setVisibility(View.VISIBLE);
+                                noPic.setVisibility(View.GONE);
+
+                                images.add(downloadUrl.toString());
+                                displayImage(downloadUrl.toString());
+                            } else {
+                                images.add(downloadUrl.toString());
+                            }
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    /**
+     * Method that displays an ImagePicker intent.
+     *
+     * @param view The ImageView holding the Meetup's picture.
+     */
+    public void selectImage(View view) {
+        // Click on image button
+        ImagePicker.pickImage(this, "Select your image:");
+
+        fab(view);
+    }
+
+    /**
      * Method to display the Place on the PlaceMapsActivity.
      */
-    private void openMapLocation()
-    {
+    private void openMapLocation() {
         Intent newDetail = new Intent(PlaceDetailsActivity.this, PlaceMapsActivity.class);
         newDetail.putExtra("latitude", latitude);
         newDetail.putExtra("longitude", longitude);
@@ -248,8 +406,7 @@ public class PlaceDetailsActivity extends BaseActivity
      *
      * @param view The TextEdit holding the Squad's name.
      */
-    public void viewSquad(View view)
-    {
+    public void viewSquad(View view) {
         //Sends the id to the details activity
         Intent detail = new Intent(PlaceDetailsActivity.this, SquadDetailActivity.class);
         detail.putExtra("squadId", place.getSquad());
@@ -259,23 +416,26 @@ public class PlaceDetailsActivity extends BaseActivity
     /**
      * Method to view the Place's Meetups.
      */
-    public void viewMeetups()
-    {
+    public void viewMeetups() {
         // Loads the MeetupsList activity displaying the Meetups that are at this place
         Intent intent = new Intent(this, MeetupsListActivity.class);
         intent.putExtra("placeId", placeId);
         startActivity(intent);
     }
 
-    public void fab(View view)
-    {
-        if (burger == false)
-        {
+    /**
+     * Method that displays editing controls for the Place if the signed-in User is the Host.
+     */
+    public void editMode() {
+        hostMenu.setVisibility(View.VISIBLE);
+    }
+
+    public void fab(View view) {
+        if (burger == false) {
             burger = true;
             burgerMenu.setVisibility(View.VISIBLE);
             fab.setImageResource(R.drawable.ic_cross);
-        } else
-        {
+        } else {
             burger = false;
             burgerMenu.setVisibility(View.GONE);
             fab.setImageResource(R.drawable.ic_burger);
--- app/src/main/java/uk/ac/tees/donut/squad/location/LocContants.java
+++ app/src/main/java/uk/ac/tees/donut/squad/location/LocContants.java
@@ -6,15 +6,19 @@ package uk.ac.tees.donut.squad.location;
 
 public final class LocContants
 {
+    //Geocode result variables
     public static final int SUCCESS_RESULT = 0;
     public static final int FAILURE_RESULT = 1;
 
+    //Variables deciding whether to geocode based on address of location
     public static final int USE_ADDRESS_NAME = 1;
     public static final int USE_ADDRESS_LOCATION = 2;
 
+    //Geocode package
     public static final String PACKAGE_NAME =
             "com.sample.foo.simplelocationapp";
     public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    //Result Variables
     public static final String RESULT_DATA_KEY = PACKAGE_NAME + ".RESULT_DATA_KEY";
     public static final String RESULT_ADDRESS = PACKAGE_NAME + ".RESULT_ADDRESS";
     public static final String LOCATION_LATITUDE_DATA_EXTRA = PACKAGE_NAME + ".LOCATION_LATITUDE_DATA_EXTRA";
